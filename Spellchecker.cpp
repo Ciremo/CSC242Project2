d80c2c2aa2a354bfb2cd59678b0170181a04f3ad
Ciremo <82509462+Ciremo@users.noreply.github.com>
2026-02-25 21:39:26 -0800

    Improved spellchecker.cpp and inputted names in input.txt

--- Spellchecker.cpp
+++ Spellchecker.cpp
@@ -6,7 +6,7 @@
 ** Assignment: 	Final Project part 1
 ** File: 		source.cpp
 ** Description: Spell Checker for the English language. It uses a dictionary
-** of words to check if a given word is speeled correctly or not. 
+** of words to check if a given word is in the dictionary or not.
 **
 ** Authors: 	Jhoana Mayle, Ever Hernandez, Eric Mounivong
 ** Date: 		February 25, 2026
@@ -20,55 +20,54 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	// STEP 1: Open the dictionary file
-	// Authored by: Jhoana Mayle
-	ifstream dict_file("words.txt");
-	if (dict_file.fail()) 
-	{
-		cout << "Could not open dictionary file." << endl;
-		return 1;
-	}
-	
-	// STEP 2: Define a vector of strings called words
-	// Authored by: Jhoana Mayle
-	vector<string> words;
-	
-	// STEP 3: Read each word from the dictionary file and store it in the vector
-	// Authored by: Eric Mounivong
-	string word;
-	while (dict_file >> word) 
-	{
-		words.push_back(word);
-	}
+    // STEP 1: Open the dictionary file.
+    // Authored by: Jhoana Mayle
+    ifstream dict_file("words.txt");
+    if (dict_file.fail())
+    {
+        cout << "Could not open dictionary file: words.txt" << endl;
+        return 1;
+    }
 
-	// STEP 4: Open the File to be checked
-	// Authored by: Ever Hernandez
-	string filename;
-	cout << "Enter the name of the file to check: ";
-	cin >> file name; 
-	
-	// STEP 5: Close the dictionary file
-	// Authored by: Eric Mounivong
-	dict_file.close();
-	
-	// STEP 6: Prompt the user to enter a word
-	// Authored by: Eric Mounivong
-	string input_word;
-	cout << "Enter a word to check: ";
-	cin >> input_word;
-	
-	// STEP 7: Check if the input word is in the vector of words
-	// Authored by: Eric Mounivong
-	if (find(words.begin(), words.end(), input_word) != words.end()) 
-		{
-		cout << "The word is spelled correctly." << endl;
-		} 
-		else
-			{
-		cout << "The word is misspelled." << endl;
-			}
-	return 0;
+    // STEP 2: Define a vector of strings called words.
+    // Authored by: Jhoana Mayle
+    vector<string> words;
+
+    // STEP 3: For each word in the dictionary file, append the word to the words vector.
+    // Authored by: Eric Mounivong
+    string word;
+    while (dict_file >> word)
+    {
+        words.push_back(word);
+    }
+
+    // Sort so we can use binary_search (faster than find)
+    // Authored by: Eric Mounivong
+    sort(words.begin(), words.end());
+
+    // STEP 4: Open the file to be checked.
+    // Authored by: Ever Hernandez
+    ifstream check_file("input.txt"); // <-- change this to the file you want to spellcheck
+    if (check_file.fail())
+    {
+        cout << "Could not open file to be checked: input.txt" << endl;
+        return 1;
+    }
+
+    // STEP 5: For each word in that file, if the word is not contained in the words vector, print the word.
+    // Authored by: Eric Mounivong
+    string input_word;
+    while (check_file >> input_word)
+    {
+        // Using binary_search since we sorted the vector
+        if (!binary_search(words.begin(), words.end(), input_word))
+        {
+            cout << input_word << endl;
+        }
+    }
+
+    return 0;
 } // End of the program
 
