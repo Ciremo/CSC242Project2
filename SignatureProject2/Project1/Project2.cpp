50c8f48054603228a8bf66208655f91e82cb1f1b
Ciremo <82509462+Ciremo@users.noreply.github.com>
2026-02-25 22:20:21 -0800

    Project setup: add configs and Source.cpp stub
    Updated project files to reference Source.cpp and Spellchecker.cpp, fixed input.txt/words.txt inclusion, and added Visual Studio filters. Created new Project1.vcxproj and filters for SignatureProject2. Added Source.cpp with basic includes and empty main function.

---
+++ SignatureProject2/Project1/Project2.cpp
@@ -0,0 +1,18 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+**
+** Class: 		CSC 242
+** Assignment: 	Final Project part 2
+** File: 		source.cpp
+** Description: 
+**
+** Authors: 	Jhoana Mayle, Ever Hernandez, Eric Mounivong
+** Date: 		February 25, 2026
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+}
